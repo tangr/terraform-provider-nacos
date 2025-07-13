78c4728749fc62869b1d834905c75ddcb9b8241e
tangshoubin <tangshoubin@itiger.com>
2025-07-13 21:36:19 +0800

    update

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,10 @@
-provider "nacos" {
-  # example configuration here
+terraform {
+  required_providers {
+    nacos = {
+      source  = "terralist.exodushk.com/tangr/nacos"
+      version = "0.0.1"
+    }
+  }
 }
+
+provider "nacos" {}
